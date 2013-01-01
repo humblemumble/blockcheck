c61b6778cdd614132294c77664bedd151240a3b2
Peter Struhár <struharp@defiant.(none)>
2013-01-01 21:19:59 +0100

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blockcheck::Application.config.secret_token = '83df9c5873ff8ea62277ff5363838299f8e20930e095ca5c1d40b36b875d71e8b5a28535fd4d8337bce00e65db78d2b2502d52ec85b34812a94d7de4c3130e02'
